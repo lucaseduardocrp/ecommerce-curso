fecab06b458b084952720a439be50af83473ef71
lucaseduardocrp <lucaseduardocrp@gmail.com>
2023-07-13 15:09:52 -0300

    update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className='mt-28'>
         <Main />
         <NextScript />
       </body>
